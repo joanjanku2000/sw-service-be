65cde39473318ba631ad823aa3f98c87e646b1c3
joanjanku2000 <janku.joan4@gmail.com>
2025-04-22 14:07:49 +0200

    modify local_setup.sh by providing a local docker compose config so everything communicates better with each other

--- local_setup.sh
+++ local_setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Start Mongo container
-docker compose up mongo_db -d
-docker compose build app
-docker compose run -e SPRING_PROFILES_ACTIVE=local -p 8181:8080 -d app
+docker compose -f docker-compose-local.yml up --build -d
+
 
 
